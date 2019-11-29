99737cea188f9cf5a351e25d67e48b863766ede2
Alexander Ratushnyi <forcewizu@gmail.com>
2019-11-30 01:17:25 +0200

    fix time

--- components/navigation/components/SNavigationInfo.jsx
+++ components/navigation/components/SNavigationInfo.jsx
@@ -2,23 +2,27 @@ import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import { clock } from '../../../utils';
 import moment from 'moment';
+// import Clock from 'react-clock'
 
 const SNavigationInfo = ({ className = ''  }) => {
-  const setCurrentTime = () => moment().format('H:mm:ss');
+  const setCurrentTime = () => moment().format('HH:mm');
 
   const [state, setState] = useState({...clock()});
-  const [time, setTime] = useState(setCurrentTime())
+  const [time, setTime] = useState(setCurrentTime());
+  const [date, setDate] = useState(new Date())
 
   useEffect(() => {
     setInterval(() => {
       setTime(setCurrentTime())
       setState(clock())
+      setDate(new Date())
     }, 1000)
   }, [])
 
   return (
     <div className={classNames('nav-info', state.type, className)}>
       <span className='nav-info__time'>{time}</span>
+      {/* <Clock value={date.date}/> */}
       <span className='nav-info__text'>{state.msg}</span>
     </div>
   )
