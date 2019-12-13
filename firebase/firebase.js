1b9a6932d674486418d8b0f75428f066806b815d
Alexander Ratushnyi <forcewizu@gmail.com>
2019-12-13 18:08:13 +0200

    :lock: security issues

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -6,14 +6,14 @@ import "firebase/firestore";
 import "firebase/analytics";
 
 const config = {
-  apiKey: "AIzaSyBejENBWHCrM16WRbh3PgJOq9NkSfI8BA8",
-  authDomain: "school23-5af03.firebaseapp.com",
-  databaseURL: "https://school23-5af03.firebaseio.com",
-  projectId: "school23-5af03",
-  storageBucket: "school23-5af03.appspot.com",
-  messagingSenderId: "769504287427",
-  appId: "1:769504287427:web:1100c2fdf81d750979e076",
-  measurementId: "G-50352WDD2R"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 if (!firebase.apps.length) {
