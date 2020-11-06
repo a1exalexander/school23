f07e5717ea351c988bdd3a453f9a69d91d1303bd
Alexander Ratushnyi <alexander.ratushnyi@gmail.com>
2020-11-06 12:17:28 +0200

    🐛 fix model

--- pages/schedule/index.jsx
+++ pages/schedule/index.jsx
@@ -1,7 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import { Table } from 'antd';
 import { Page } from '../../components';
-import { columnsModel } from './model';
+
+export const columnsModel = [
+  {
+    title: 'День тижня',
+    width: '140px',
+    dataIndex: 'name',
+    key: 'name',
+    fixed: 'left'
+  },
+  {
+    title: 'Урок',
+    width: '110px',
+    dataIndex: 'lessons',
+    key: 'lessons',
+    fixed: 'left'
+  },
+  ...[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((i) => ({
+    title: `${i} клас`,
+    children: [
+      {
+        title: `${i}-А`,
+        width: 220,
+        dataIndex: `${i}а`,
+        key: `${i}а`,
+        ellipsis: {
+          showTitle: false
+        }
+      },
+      {
+        title: `${i}-Б`,
+        width: 220,
+        dataIndex: `${i}б`,
+        key: `${i}б`,
+        ellipsis: {
+          showTitle: false
+        }
+      },
+      {
+        title: `${i}-В`,
+        width: 220,
+        dataIndex: `${i}в`,
+        key: `${i}в`,
+        ellipsis: {
+          showTitle: false
+        }
+      }
+    ]
+  }))
+];
 
 const weekDays = ['Понеділок', 'Вівторок', 'середа', 'четверг', "п'ятниця", 'субота'];
 
