5f74a54ddef16ea976c28001ab9c5c04138e26ed
Alexander Ratushnyi <alexander.ratushnyi@gmail.com>
2020-11-06 13:33:38 +0200

    🐛 Update config deps

--- next.config.js
+++ next.config.js
@@ -2,39 +2,38 @@ const withSass = require('@zeit/next-sass');
 const withImages = require('next-images');
 const withCSS = require('@zeit/next-css');
 const compose = require('next-compose-plugins');
-const getPathsObject = require('./scripts/getPathsObject');
-// const dotEnvResult = require('dotenv').config();
 const nextEnv = require('next-env');
 const dotenvLoad = require('dotenv-load');
+const getPathsObject = require('./scripts/getPathsObject');
 
 dotenvLoad();
 
 const withNextEnv = nextEnv();
 
 const nextConfig = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  webpack: (config, { webpack }) => {
     config.module.rules.push({
       test: /\.js$/,
       exclude: /node_modules(?!\/quill-image-drop-module|quill-image-resize-module|quill-image-rotate-module)/,
-      loader: 'babel-loader',
+      loader: 'babel-loader'
     });
     config.plugins.push(
       new webpack.ProvidePlugin({
-        'window.Quill': 'quill',
+        'window.Quill': 'quill'
       })
     );
     return config;
   },
-  exportPathMap: function () {
+  exportPathMap: () => {
     const fileObj = getPathsObject();
     return {
       ...fileObj,
       '/': { page: '/' },
       '/index': { page: '/' },
-      '/news/index': { page: '/news' },
+      '/news/index': { page: '/news' }
     };
   },
-  images: { domains: ['localhost', 'firebasestorage.googleapis.com', 'https://school23.now.sh'] },
+  images: { domains: ['localhost', 'firebasestorage.googleapis.com', 'https://school23.now.sh'] }
 };
 
 module.exports = compose([[withImages], [withSass], [withCSS], [withNextEnv]], nextConfig);
