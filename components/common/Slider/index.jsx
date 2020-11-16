2a8aa57d1d992464126b8b9dbd2ae63b0717707a
Alexander Ratushnyi <alexander.ratushnyi@gmail.com>
2020-11-16 10:22:35 +0200

    fix slider

--- components/common/Slider/index.jsx
+++ components/common/Slider/index.jsx
@@ -3,18 +3,10 @@ import React, { useState } from 'react';
 import Carousel from 'react-slick';
 import classNames from 'classnames';
 import { arrayOf, oneOfType, shape, string, bool } from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import GridContainer from './components/GridContainer';
-import GridItem from './components/GridItem';
-import Card from './components/Card';
-import carouselStyle from './styles/carouselStyle';
 import './styles/_plugin-react-slick.scss';
 import { SFullScreenImage } from '../media/SFullScreenImage';
 
-const useStyles = makeStyles(carouselStyle);
-
 export default function Slider({ slides, className, autoplay, title }) {
-  const classes = useStyles();
   const [fullImage, setFullImage] = useState();
 
   const renderSlides = () => {
@@ -44,23 +36,10 @@ export default function Slider({ slides, className, autoplay, title }) {
   return (
     <div className={classNames('Slider', className)} id="carousel">
       <SFullScreenImage src={fullImage} onClose={() => setFullImage(null)} />
-      <div className={classes.container}>
-        <GridContainer>
-          <GridItem xs={12} sm={10} md={10} className={classes.marginAuto}>
-            <Card>
-              <Carousel
-                dots
-                infinite
-                speed={500}
-                slidesToShow={1}
-                slidesToScroll={1}
-                autoplay={autoplay}
-              >
-                {renderSlides()}
-              </Carousel>
-            </Card>
-          </GridItem>
-        </GridContainer>
+      <div className="Slider__card">
+        <Carousel dots infinite speed={500} slidesToShow={1} slidesToScroll={1} autoplay={autoplay}>
+          {renderSlides()}
+        </Carousel>
       </div>
     </div>
   );
